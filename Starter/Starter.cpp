6837319ff34e068e791dc5265d2df08bd6208bc8
因幡组员13号 <madhatter1800@qq.com>
2022-08-27 11:23:42 +0800

    ppm 格式输出，并运行

--- Starter/Starter.cpp
+++ Starter/Starter.cpp
@@ -3,18 +3,42 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+//<<输出一张PPM格式的图片>>
+void drawOnePPM() {
+//<<设置Image的长度与宽度，以像素为单位>>
+#pragma region ImageSize 
+	const int image_width = 256;
+	const int image_height = 256;
+#pragma endregion
+
+//<<按照PPM的格式打印字符串>>
+/*
+*	期望输出: 红色从左到右从完全关闭（黑色）变为完全打开（亮红色），
+			绿色变为 从底部的黑色到顶部的完全打开。 红色和绿色一起变成黄色，
+			所以我们 应该期望右上角是黄色的。 
+*/
+#pragma region RenderPart
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; --j) {
+		for (int i = 0; i < image_width; ++i) {
+			auto r = double(i) / (image_width - 1);
+			auto g = double(j) / (image_height - 1);
+			auto b = 0.25;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+#pragma endregion
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+int main()
+{
+	drawOnePPM();
+}
