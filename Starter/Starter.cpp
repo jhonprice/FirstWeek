c2904d98bcb4ef2eb66a5b29f17f7f4c4e6ee1fb
因幡组员13号 <madhatter1800@qq.com>
2022-08-27 11:27:01 +0800

    添加进度指示

--- Starter/Starter.cpp
+++ Starter/Starter.cpp
@@ -20,8 +20,9 @@ void drawOnePPM() {
 */
 #pragma region RenderPart
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+	//<<添加进度指示的渲染循环>> - 1
 	for (int j = image_height - 1; j >= 0; --j) {
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; //1
 		for (int i = 0; i < image_width; ++i) {
 			auto r = double(i) / (image_width - 1);
 			auto g = double(j) / (image_height - 1);
@@ -33,6 +34,7 @@ void drawOnePPM() {
 
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
+		std::cerr << "\nDone.\n";  //1
 	}
 #pragma endregion
 
