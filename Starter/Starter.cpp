8869034505e9c511f94fc7285c93ce2a8cdfad79
因幡组员13号 <madhatter1800@qq.com>
2022-08-27 12:05:04 +0800

    设置工具类，简化main函数，start项目结束

--- Starter/Starter.cpp
+++ Starter/Starter.cpp
@@ -2,7 +2,8 @@
 //
 
 #include <iostream>
-
+#include "vec3.h" //2
+#include "color.h" //2
 
 //<<输出一张PPM格式的图片>>
 void drawOnePPM() {
@@ -21,6 +22,7 @@ void drawOnePPM() {
 #pragma region RenderPart
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 	//<<添加进度指示的渲染循环>> - 1
+	//<<利用头文件vec3 color工具函数简化过程>> - 2
 	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; //1
 		for (int i = 0; i < image_width; ++i) {
@@ -28,11 +30,15 @@ void drawOnePPM() {
 			auto g = double(j) / (image_height - 1);
 			auto b = 0.25;
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
+			//2 int ir = static_cast<int>(255.999 * r);
+			//2 int ig = static_cast<int>(255.999 * g);
+			//2 int ib = static_cast<int>(255.999 * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			//2 std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+
+			color pixel_color(r,g,b); //2
+			write_color(std::cout, pixel_color); //2
+			
 		}
 		std::cerr << "\nDone.\n";  //1
 	}
---
+++ Starter/vec3.h
@@ -0,0 +1,109 @@
+#pragma once
+#include<cmath>
+#include<iostream>
+
+using std::sqrt;
+
+//<<3D向量的定义>>
+class vec3 {
+public:
+	//<<初始化部分>>
+	vec3():e{0,0,0}{}
+	vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+
+	//<<分量访问部分>>
+	double x() const { return e[0]; }
+	double y() const { return e[1]; }
+	double z() const { return e[2]; }
+
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	double operator[](int i) const { return e[i]; }
+	double& operator[](int i) { return e[i]; }
+//<<常用操作符: += *= /= 长度，长度的平方>>
+#pragma region OpPart
+    vec3& operator+=(const vec3& v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3& operator*=(const double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3& operator/=(const double t) {
+        return *this *= 1 / t;
+    }
+
+    double length() const {
+        return sqrt(length_squared());
+    }
+
+    double length_squared() const {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+#pragma endregion
+
+    
+
+public:
+	double e[3];
+};
+
+//<<为vec3的同类设置别名>>
+using point3 = vec3;
+using color = vec3;
+
+
+//<<常用函数部分：输出，运算，点乘，叉乘，单位化>>
+#pragma region FuncPart
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+    return u.e[0] * v.e[0]
+        + u.e[1] * v.e[1]
+        + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
+#pragma endregion
+
+
