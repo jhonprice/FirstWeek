2f9435889799d066c40b55de6d35166334090679
因幡组员13号 <madhatter1800@qq.com>
2022-08-28 14:33:12 +0800

    折射，全内反射，石里克近似材质，Dielectrics结束

--- Metal/material.h
+++ Metal/material.h
@@ -57,3 +57,53 @@ public:
     color albedo;
     double fuzz=0;
 };
+
+//<<折射材质>> - 3
+//+=<<全内反射>> - 4
+//+=<<石里克近似>> - 5
+class dielectric : public material {
+public:
+    dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    virtual bool scatter(
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        //vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio); // 3
+        //scattered = ray(rec.p, refracted); // 3
+
+// 4
+#pragma region MyRegion
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        //5: if (cannot_refract)
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double()) //5
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+#pragma endregion
+
+
+        return true;
+    }
+
+public:
+    double ir; // Index of Refraction
+private:
+    // 4
+    static double reflectance(double cosine, double ref_idx) {
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
+};
