b50580da01d3b59d612e765b18ffca8556698708
因幡组员13号 <madhatter1800@qq.com>
2022-10-17 14:53:54 +0800

    2. 输出图像

--- Starter/Starter.cpp
+++ Starter/Starter.cpp
@@ -1,20 +1,66 @@
 ﻿// Starter.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <array>
+#include <chrono>
+#include "../include/stb_image_write.h"
+using namespace std::chrono;
+
+//Image的大小（像素
+const int imageCW = 1280;
+const int imageCH = 720;
+const int length = imageCW * imageCH * 3;
+std::array<unsigned char, length> result{ 0 };
+const double maxColorParam = 255.999;
+
+struct Color {
+    int r;
+    int g;
+    int b;
+};
+
+void setPix(int x, int y, Color color) {
+    result[(x * imageCW + y) * 3 + 0] = color.r;
+    result[(x * imageCW + y) * 3 + 1] = color.g;
+    result[(x * imageCW + y) * 3 + 2] = color.b;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    // Render：输出一张图片，列变化控制r分量，行变化控制g分量，b分量保持0.25
+    // 默认从上到下，从左到右
+
+    auto start = system_clock::now();
+    for (int x{ 0 }; x < imageCH; x++) {
+        std::cout << "\rScanlines remaining: " << x << ' ' << std::flush;
+        for (int y{ 0 }; y < imageCW; y++) {
+            auto r = double(y) / (imageCW - 1);
+            auto g = double(x) / (imageCH - 1);
+            auto b = 0.25;
+
+            r *= maxColorParam;
+            g *= maxColorParam;
+            b *= maxColorParam;
+
+            setPix(x, y, { static_cast<int>(r),static_cast<int>(g), static_cast<int>(b) });
+  
+        }
+    }
+    auto end = system_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+
+    //写入文件
+    // 默认从上到下，从左到右,现在翻转（00为左下角
+    stbi_flip_vertically_on_write(1);
+    stbi_write_tga("./test.tga", imageCW, imageCH, 3, result.data());
+
+    //报告完成
+    std::cout << "\nDone: "<< double(duration.count()) * microseconds::period::num / microseconds::period::den<<" s" << std::endl;
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
