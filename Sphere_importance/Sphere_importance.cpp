e98c00d469d3f9d2e74f11b98fb0cc50f42cc8fb
因幡组员13号 <madhatter1800@qq.com>
2022-10-21 15:32:06 +0800

    4 方向球上的MC积分

---
+++ Sphere_importance/Sphere_importance.cpp
@@ -0,0 +1,21 @@
+﻿// Sphere_importance.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../Starter/vec3.h"
+#include <iomanip>
+inline double pdf(const Vec3& p) {
+    return 1 / (4 * pi);
+}
+
+int main() {
+    int N = 1000000;
+    auto sum = 0.0;
+    for (int i = 0; i < N; i++) {
+        Vec3 d = random_unit_vector();
+        auto cosine_squared = d.z() * d.z();
+        sum += cosine_squared / pdf(d);
+    }
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "I = " << sum / N << '\n';
+}
