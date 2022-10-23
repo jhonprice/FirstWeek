f3c2a2a713b35fad6126e186b5bf5c9aca0adea8
因幡组员13号 <madhatter1800@qq.com>
2022-10-23 14:40:54 +0800

    7 生成随机方向

---
+++ HemisphereUniformSampling/HemisphereUniformSampling.cpp
@@ -0,0 +1,54 @@
+﻿// HemisphereUniformSampling.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../Starter/math_helper.h"
+#include <iomanip>
+#include "..\Starter\vec3.h"
+
+void hemisphereUni() {
+    int N = 1000000;
+    auto sum = 0.0;
+    for (int i = 0; i < N; i++) {
+        auto r1 = random_double();
+        auto r2 = random_double();
+        auto x = cos(2 * pi * r1) * 2 * sqrt(r2 * (1 - r2));
+        auto y = sin(2 * pi * r1) * 2 * sqrt(r2 * (1 - r2));
+        auto z = 1 - r2;
+        sum += z * z * z / (1.0 / (2.0 * pi));
+    }
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "Pi/2     = " << pi / 2 << '\n';
+    std::cout << "Estimate = " << sum / N << '\n';
+}
+
+
+inline Vec3 random_cosine_direction() {
+    auto r1 = random_double();
+    auto r2 = random_double();
+    auto z = sqrt(1 - r2);
+
+    auto phi = 2 * pi * r1;
+    auto x = cos(phi) * sqrt(r2);
+    auto y = sin(phi) * sqrt(r2);
+
+    return Vec3(x, y, z);
+}
+
+void hemisphereDensity() {
+    int N = 1000000;
+
+    auto sum = 0.0;
+    for (int i = 0; i < N; i++) {
+        auto v = random_cosine_direction();
+        sum += v.z() * v.z() * v.z() / (v.z() / pi);
+    }
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "Pi/2     = " << pi / 2 << '\n';
+    std::cout << "Estimate = " << sum / N << '\n';
+}
+int main() {
+    hemisphereUni();
+    hemisphereDensity();
+}
---
+++ RandomDir/RandomDir.cpp
@@ -0,0 +1,32 @@
+﻿// RandomDir.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "../Starter/math_helper.h"
+
+void random_cosine_direction()
+{
+    double r1 = random_double();
+    double r2 = random_double();
+    double z = sqrt(1 - r2);
+    double x = cos(2 * pi * r1) * 2 * sqrt(r2);
+    double y = sin(2 * pi * r1) * 2 * sqrt(r2);
+    std::cout << x << "," << y << "," << z << '\n';
+}
+
+void random_sphere() {
+    auto r1 = random_double();
+    auto r2 = random_double();
+    auto x = cos(2 * pi * r1) * 2 * sqrt(r2 * (1 - r2));
+    auto y = sin(2 * pi * r1) * 2 * sqrt(r2 * (1 - r2));
+    auto z = 1 - 2 * r2;
+    std::cout << x << "," << y << "," << z << '\n';
+}
+
+int main() {
+    std::cout << 'A' << "," << 'B' << "," << 'C' << '\n';
+    for (int i = 0; i < 200; i++) {
+        //random_sphere();
+        random_cosine_direction();
+    }
+}
